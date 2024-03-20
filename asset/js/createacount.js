929b2042c857d77bb7e4a33f6bbaa1a3aedea100
khawla-Moh <khawlaMoh00@gmail.com>
2024-03-20 16:58:27 +0300

    edit font

--- asset/js/createacount.js
+++ asset/js/createacount.js
@@ -3,3 +3,15 @@ $(function(){
         modules : 'security'
     });
 })
+
+
+$(document).ready(function() {
+    // Initialize form validation
+    $.validate({
+      form: '#myForm',
+      onSuccess: function() {
+        // Display alert on successful form submission
+        alert('Account created successfully!');
+      }
+    });
+  });
